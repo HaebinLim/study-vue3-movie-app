378b68f7276883e4f5901932ecbbe8adc9055fb5
haebin <goguma-guma@hanmail.net>
2022-03-08 19:36:05 +0900

    220308 fastcampus vue3-movie-app api 비동기처리

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
     <div class="inner">
+      <div class="message">
+        {{ message }}
+      </div>
       <MovieItem
         v-for="item in movies"
         :key="item.imdbID"
@@ -20,6 +23,9 @@ export default {
     // movies는 처음 빈 배열이고, searchMovies에서 갱신이 된 후 반응성이 유지된 상태로? 활용되기 때문에? computed에 정의한다 @.@ 
     movies() {
       return this.$store.state.movie.movies
+    },
+    message() {
+      return this.$store.state.movie.message
     }
   },
 }
--- src/store/movie.js
+++ src/store/movie.js
@@ -38,36 +38,62 @@ export default {
 		// 데이터 수정 안됨. 비동기 처리하는 곳
 	//searchMovies({ state, getters, commit }){
 		async searchMovies(context, payload){
-			const { title, type, number, year } = payload;
-			const OMDB_API_KEY = '7035c60c';
-      const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&$page=1`);
-			const { Search, totalResults } = res.data;
-			context.commit('updateState', {
-				movies: _uniqBy(Search, 'imdbID'),
-				message: 'hi',
-				loading: true,
-			});
-			console.log(totalResults);
-			console.log(typeof totalResults);
+			try {
+				const res = await _fetchMovie({
+					...payload, 
+					page: 1
+				});
+				const { Search, totalResults } = res.data;
+				context.commit('updateState', {
+					movies: _uniqBy(Search, 'imdbID'),
+				});
+				console.log(totalResults);
+				console.log(typeof totalResults);
 
-			const total = parseInt(totalResults, 10);
-			const pageLength = Math.ceil(total / 10);
+				const total = parseInt(totalResults, 10);
+				const pageLength = Math.ceil(total / 10);
 
-			if(pageLength > 1) {
-				for (let page = 2; page <= pageLength; page += 1){
-					if(page > (number / 10)) break;
+				if(pageLength > 1) {
+					for (let page = 2; page <= pageLength; page += 1){
+						if(page > (payload.number / 10)) break;
 
-					const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&$page=${page}`);
-					const { Search } = res.data;
-					context.commit('updateState', {
-						movies: [
-							...context.state.movies, 
-							..._uniqBy(Search, 'imdbID')
-						],
-					})	
+						const res = await _fetchMovie({
+							...payload, 
+							page
+						});
+						const { Search } = res.data;
+						context.commit('updateState', {
+							movies: [
+								...context.state.movies, 
+								..._uniqBy(Search, 'imdbID')
+							],
+						})	
+					}
 				}
+			} catch (message) {
+				context.commit('updateState', {
+					movies: [],
+					message
+				})
 			}
-
 		}
 	},
+}
+
+// _ 현재 파일에서만 사용한다는 뜻?
+function _fetchMovie(payload){
+	const { title, type, year, page } = payload;
+	const OMDB_API_KEY = '7035c60c';
+	const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&year=${year}&page=${page}`;
+
+	return new Promise((resolve, reject) => {
+		axios.get(url)
+			.then((res) => {
+				if(res.data.Error){
+					reject(res.data.Error)
+				}
+				resolve(res)
+			})
+			.catch((err) => reject(err.message))
+	})
 }
